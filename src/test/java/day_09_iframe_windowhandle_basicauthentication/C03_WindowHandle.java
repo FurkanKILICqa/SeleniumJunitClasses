00b3af7e5b540d5ca20027311942f3aa6a4a44ed
Furkan Kılıc <fkrx928@gmail.com>
2023-11-09 13:44:40 +0300

    my commit

--- src/test/java/day_09_iframe_windowhandle_basicauthentication/C03_WindowHandle.java
+++ src/test/java/day_09_iframe_windowhandle_basicauthentication/C03_WindowHandle.java
@@ -111,4 +111,75 @@ public class C03_WindowHandle extends TestBase {
     }
 
 
+    @Test
+    public void test03() {
+        //  https://the-internet.herokuapp.com/windows adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/windows");
+
+        //  ilk sayfadaki textin "Opening a new window" olduğunu test edin.
+        String text = driver.findElement(By.xpath("//h3")).getText();
+        Assert.assertEquals("Opening a new window",text);
+
+        //  ilk sayfa Title'ının "The Internet" olduğunu test edin.
+        Assert.assertEquals("The Internet",driver.getTitle());
+
+
+        //  "Click Here" butonuna tıklayın.
+        driver.findElement(By.xpath("(//a)[2]")).click();
+
+        //  ikinci sayfa Title'ının "New Window" olduğunu test edin.
+
+        driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+        Assert.assertEquals("New Window",driver.getTitle());
+
+        waitForSecond(2);
+
+        //  ilk sayfaya dönün ve Title'ının "The Internet" olduğunu test edin.
+        driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
+        Assert.assertEquals("The Internet",driver.getTitle());
+        waitForSecond(2);
+
+        //  ikinci sayfaya tekrar geçin.
+        driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+        waitForSecond(2);
+
+        //  ilk sayfaya tekrar dönün.
+        driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
+    }
+
+    @Test
+    public void test04() {
+        //  https://the-internet.herokuapp.com/windows adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/windows");
+
+        //  ilk sayfadaki textin "Opening a new window" olduğunu test edin.
+        String text = driver.findElement(By.xpath("//h3")).getText();
+        Assert.assertEquals("Opening a new window",text);
+
+        //  ilk sayfa Title'ının "The Internet" olduğunu test edin.
+        Assert.assertEquals("The Internet",driver.getTitle());
+
+
+        //  "Click Here" butonuna tıklayın.
+        driver.findElement(By.xpath("(//a)[2]")).click();
+
+        //  ikinci sayfa Title'ının "New Window" olduğunu test edin.
+        window(1);
+        Assert.assertEquals("New Window",driver.getTitle());
+        waitForSecond(2);
+
+        //  ilk sayfaya dönün ve Title'ının "The Internet" olduğunu test edin.
+        window(0);
+        Assert.assertEquals("The Internet",driver.getTitle());
+        waitForSecond(2);
+
+        //  ikinci sayfaya tekrar geçin.
+        window(1);
+        waitForSecond(2);
+
+        //  ilk sayfaya tekrar dönün.
+        window(0);
+
+    }
 }
+
