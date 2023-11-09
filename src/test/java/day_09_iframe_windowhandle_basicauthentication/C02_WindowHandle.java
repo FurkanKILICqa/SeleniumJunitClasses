f81bc9cc4f8e2ceb6801a8e2f7f530e3feb44c05
Furkan Kılıc <fkrx928@gmail.com>
2023-11-09 12:25:36 +0300

    my commit

--- src/test/java/day_09_iframe_windowhandle_basicauthentication/C02_WindowHandle.java
+++ src/test/java/day_09_iframe_windowhandle_basicauthentication/C02_WindowHandle.java
@@ -21,20 +21,101 @@ public class C02_WindowHandle extends TestBase {
     public void test01() {
 
         driver.get("https://www.techproeducation.com");
-        driver.getWindowHandle();
+        String techproWindowHandleDegeri = driver.getWindowHandle();
+
+        Assert.assertEquals("TechPro Education",driver.getTitle());
+
+
+        //Window 2'de (yeni bir pencerede) https://www.youtube.com sayfasını açınız:
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://www.youtube.com");
+        String youtubeWindowHandleDegeri = driver.getWindowHandle();
+
+
+        //Window 3'te (yeni bir pencerede) https://www.linkedin.com sayfasını açınız:
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://www.linkedin.com");
+        String linkedinWindowHandleDegeri = driver.getWindowHandle();
+
+
+        waitForSecond(2);
+        //techproeducation sayfasına geçiniz:
+        driver.switchTo().window(techproWindowHandleDegeri);
+        waitForSecond(2);
+
+        //youtube sayfasına geçiniz:
+        driver.switchTo().window(youtubeWindowHandleDegeri);
+        waitForSecond(2);
+
+        //linkedIn sayfasına geçiniz:
+        driver.switchTo().window(linkedinWindowHandleDegeri);
+        waitForSecond(2);
+
+    }
+
+    @Test
+    public void test02() {
+        driver.get("https://www.techproeducation.com");
+        String techproWindowHandleDegeri = driver.getWindowHandle();
+
+        Assert.assertEquals("TechPro Education",driver.getTitle());
+
+
+        //Window 2'de (yeni bir pencerede) https://www.youtube.com sayfasını açınız:
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.youtube.com");
+        String youtubeWindowHandleDegeri = driver.getWindowHandle();
+
+
+        //Window 3'te (yeni bir pencerede) https://www.linkedin.com sayfasını açınız:
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.linkedin.com");
+        String linkedinWindowHandleDegeri = driver.getWindowHandle();
+
+
+        waitForSecond(2);
+        //techproeducation sayfasına geçiniz:
+        driver.switchTo().window(techproWindowHandleDegeri);
+        waitForSecond(2);
+
+        //youtube sayfasına geçiniz:
+        driver.switchTo().window(youtubeWindowHandleDegeri);
+        waitForSecond(2);
+
+        //linkedIn sayfasına geçiniz:
+        driver.switchTo().window(linkedinWindowHandleDegeri);
+        waitForSecond(2);
+    }
+
+
+    @Test
+    public void test03() {
+        driver.get("https://www.techproeducation.com");
 
 
         Assert.assertEquals("TechPro Education",driver.getTitle());
 
 
-        driver.switchTo().newWindow(WindowType.WINDOW);
+        //Window 2'de (yeni bir pencerede) https://www.youtube.com sayfasını açınız:
+        driver.switchTo().newWindow(WindowType.TAB);
         driver.get("https://www.youtube.com");
-        String youtubeWindowHandle = driver.getWindowHandle();
 
 
 
-        driver.switchTo().newWindow(WindowType.WINDOW);
+        //Window 3'te (yeni bir pencerede) https://www.linkedin.com sayfasını açınız:
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.linkedin.com");
 
+
+        //techproeducation sayfasına geçiniz:
+
+
+
+        //youtube sayfasına geçiniz:
+
+
+
+        //linkedIn sayfasına geçiniz:
 
 
 
