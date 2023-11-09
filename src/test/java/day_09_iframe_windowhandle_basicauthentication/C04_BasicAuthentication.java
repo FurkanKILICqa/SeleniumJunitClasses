7e3c55954ca0edd976c972380ca693ee877137eb
Furkan Kılıc <fkrx928@gmail.com>
2023-11-09 20:45:59 +0300

    my commit

--- src/test/java/day_09_iframe_windowhandle_basicauthentication/C04_BasicAuthentication.java
+++ src/test/java/day_09_iframe_windowhandle_basicauthentication/C04_BasicAuthentication.java
@@ -1,6 +1,8 @@
 package day_09_iframe_windowhandle_basicauthentication;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utilities.TestBase;
 
 public class C04_BasicAuthentication extends TestBase {
@@ -16,7 +18,22 @@ public class C04_BasicAuthentication extends TestBase {
 
     @Test
     public void test01() {
+
         driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+
+
+        //    Congratulations! You must have the proper credentials. yazının çıktığını doğrulayın
+        String actualText = driver.findElement(By.xpath("//p")).getText();
+        String expectedText="Congratulations! You must have the proper credentials.";
+        Assert.assertEquals(expectedText,actualText);
+
+        //Elemental Selenium linkine tıklayınız
+        driver.findElement(By.xpath("(//a)[2]")).click();
+        window(1);//Testbase classinda olusturdugumuz window methodu ile yeni acilan sayfaya kolaylikla gecebildik
+
+        //Başlığın Elemental Selenium içerdiğini test edelim
+        Assert.assertTrue(driver.getTitle().contains("Elemental Selenium"));
+
 
 
 
