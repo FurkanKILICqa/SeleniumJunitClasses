8f823a3346798b9ababbb87914a1433814e8a4a0
Furkan Kılıc <fkrx928@gmail.com>
2023-11-09 12:46:15 +0300

    my commit

--- src/test/java/day_09_iframe_windowhandle_basicauthentication/C03_WindowHandle.java
+++ src/test/java/day_09_iframe_windowhandle_basicauthentication/C03_WindowHandle.java
@@ -1,6 +1,46 @@
 package day_09_iframe_windowhandle_basicauthentication;
 
-public class C03_WindowHandle {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class C03_WindowHandle extends TestBase {
+
+    //  https://the-internet.herokuapp.com/windows adresine gidin.
+    //  ilk sayfasının Handle degerini alın yazdırın
+    //  ilk sayfadaki textin "Opening a new window" olduğunu test edin.
+    //  ilk sayfa Title'ının "The Internet" olduğunu test edin.
+    //  "Click Here" butonuna tıklayın.
+    //  ikinci sayfa Title'ının "New Window" olduğunu test edin.
+    //  ilk sayfaya dönün ve Title'ının "The Internet" olduğunu test edin.
+    //  ikinci sayfaya tekrar geçin.
+    //  ilk sayfaya tekrar dönün.
+
+    @Test
+    public void test01() {
+
+        driver.get("https://the-internet.herokuapp.com/windows");
+
+
+        String ilkSayfaHandle = driver.getWindowHandle();
+
+
+       String text = driver.findElement(By.xpath("//h3")).getText();
+
+        Assert.assertEquals("Opening a new window",text);
+
+
+
+
+    }
+
+
+
+
+
+
+
 
 
 }
