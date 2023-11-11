726d25a46bb2c08f815b37608dd89e87e578b2fe
Furkan Kılıc <fkrx928@gmail.com>
2023-11-11 03:46:17 +0300

    my commit

---
+++ src/test/java/day_09_iframe_windowhandle_basicauthentication/PazarProjesi.java
@@ -0,0 +1,60 @@
+package day_09_iframe_windowhandle_basicauthentication;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class PazarProjesi extends TestBase {
+
+
+    //dmauri.keyden@forkshape.com
+    //keyden1234
+
+    @Test
+    public void test01() {
+        //https://www.hackerrank.com/ sitesine git
+        driver.get("https://www.hackerrank.com/");
+
+
+        //Log-in butonuna tıkla
+        driver.findElement(By.xpath("(//a)[29]")).click();
+
+
+        //For developers koşulu ile Log-in yap
+        driver.findElement(By.xpath("//*[@class='hr_button']")).click();
+
+
+        //Your username or email kutusuna kayıtlı E-mail adresini gir
+       WebElement nameBox = driver.findElement(By.id("input-1"));
+       nameBox.sendKeys("dmauri.keyden@forkshape.com");
+
+
+        //Your password kutusuna kayıtlı şifre gir
+       WebElement passwordBox = driver.findElement(By.id("input-2"));
+       passwordBox.sendKeys("keyden1234");
+
+
+       //Remember me kutusunu eğer işaterli değilse işaretle
+       WebElement rememberMe = driver.findElement(By.className("checkbox-input"));
+
+      if (!rememberMe.isSelected()){
+
+          rememberMe.click();
+      }
+        Assert.assertTrue(rememberMe.isSelected());
+
+        //Log-in butonuna tıkla
+       WebElement logIn =  driver.findElement(By.xpath("(//*[@class='ui-text'])[2]"));
+       logIn.click();
+
+
+
+
+
+
+
+
+    }
+}
