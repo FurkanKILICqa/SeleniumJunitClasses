a618eeeff3bb022ff1eb57b964445aa44fe2c82c
Furkan Kılıc <fkrx928@gmail.com>
2023-11-13 11:27:52 +0300

    my commit

--- src/test/java/day_10_cookies_actions/C02_ActionsTest.java
+++ src/test/java/day_10_cookies_actions/C02_ActionsTest.java
@@ -12,13 +12,6 @@ import javax.swing.*;
 public class C02_ActionsTest extends TestBase {
 
 
-
-
-
-
-
-
-
     @Test
     public void test01() {
 
@@ -27,20 +20,16 @@ public class C02_ActionsTest extends TestBase {
 
 
         //Kutuya sag tıklayın
-        WebElement box = driver.findElement(By.id("hot-spot"));
-
-
-        //Kutuya sag tıklayın
+        WebElement box = driver.findElement(By.cssSelector("#hot-spot"));
         Actions actions = new Actions(driver);
         actions.contextClick(box).perform();
+        //Kullandigimiz action i isleme almasi icin perform() methodunu kullanmamiz gerekiyor
 
 
         //Alert'te cikan yazinin"You selected a context menu"oldugunu test edin
-        String acpetedResult = "You selected a context manu";
-        String actualResult = driver.switchTo().alert().getText();
-        Assert.assertEquals("Actual Result ile Expected Result uyumlu degil",acpetedResult,actualResult);
-
-
+        String expectedResult ="You selected a context menu";
+        String actualdResult = driver.switchTo().alert().getText();
+        Assert.assertEquals("Actual Result ile Expected Result uyumlu degil",expectedResult,actualdResult);
 
         //Tamam diyerek alert'i kapatın
         driver.switchTo().alert().accept();
@@ -49,7 +38,31 @@ public class C02_ActionsTest extends TestBase {
 
 
 
+
     }
+
+    ////https://the-internet.herokuapp.com/context_menu sitesine gidin
+    //        driver.get("https://the-internet.herokuapp.com/context_menu");
+    //
+    //
+    //        //Kutuya sag tıklayın
+    //        WebElement box = driver.findElement(By.id("hot-spot"));
+    //
+    //
+    //        //Kutuya sag tıklayın
+    //        Actions actions = new Actions(driver);
+    //        actions.contextClick(box).perform();
+    //
+    //
+    //        //Alert'te cikan yazinin"You selected a context menu"oldugunu test edin
+    //        String acpetedResult = "You selected a context manu";
+    //        String actualResult = driver.switchTo().alert().getText();
+    //        Assert.assertEquals("Actual Result ile Expected Result uyumlu degil",acpetedResult,actualResult);
+    //
+    //
+    //
+    //        //Tamam diyerek alert'i kapatın
+    //        driver.switchTo().alert().accept();
 
 
 
---
+++ src/test/java/day_11_actions_faker/C01_ActionsTest.java
@@ -0,0 +1,48 @@
+package day_11_actions_faker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C01_ActionsTest extends TestBase {
+
+
+
+
+
+    //Acilan sayfanin Title in "Your Account" icerdigini dogrula
+
+    @Test
+    public void test01() {
+
+        //Amazon a gidelim https://www.amazon.com/
+        driver.get("https://www.amazon.com/");
+
+
+
+        //Sag ust bolumde bulunan "Account & Lists" menüsüne git  "Account" secenegine tikla
+        Actions actions = new Actions(driver);
+       WebElement accountList = driver.findElement(By.cssSelector("#nav-link-accountList"));
+
+        actions.moveToElement(accountList).perform();
+
+
+        driver.findElement(By.xpath("//span[.='Account']")).click();
+
+
+        //Acilan sayfanin Title in "Your Account" icerdigini dogrula
+        String expectedData ="Your Account";
+        String actualData=driver.getTitle();
+        Assert.assertEquals("Actual data ile Expected Data uyumlu degil",expectedData,actualData);
+
+
+
+
+    }
+
+
+
+}
