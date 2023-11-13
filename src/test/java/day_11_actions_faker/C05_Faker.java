8ab76470dc156ecb6f368f26749de2864e96e664
Furkan Kılıc <fkrx928@gmail.com>
2023-11-13 13:34:23 +0300

    my commit

--- src/test/java/day_11_actions_faker/C05_Faker.java
+++ src/test/java/day_11_actions_faker/C05_Faker.java
@@ -7,9 +7,9 @@ import utilities.TestBase;
 public class C05_Faker  {
 
 
-    //faker objesi ile fake bir soyisim yazdırınız
-    //faker objesi ile fake bir adress yazdırınız
-    //faker objesi ile fake bir telefon No yazdırınız
+
+
+
     //faker objesi ile fake bir email yazdırınız
     //faker objesi ile fake bir rastgele 15 haneli bir sayi yazdırınız
     //faker objesi ile fake bir isimsoyisim yazdırınız
@@ -23,6 +23,16 @@ public class C05_Faker  {
         //faker objesi ile fake bir isim yazdırınız
        String firstName = faker.name().firstName();
         System.out.println("firstName = " + firstName);
+
+
+        //faker objesi ile fake bir soyisim yazdırınız
+        System.out.println("LastName = " + faker.name().lastName());
+
+        //faker objesi ile fake bir adress yazdırınız
+        System.out.println("FullAddress = " + faker.address().fullAddress());
+
+        //faker objesi ile fake bir telefon No yazdırınız
+        System.out.println("CellPhone = " + faker.phoneNumber().cellPhone());
 
 
 
