b8682487d18bd8823882863716455f8b3b46f501
Furkan Kılıc <fkrx928@gmail.com>
2023-11-14 13:21:34 +0300

    my commit

--- src/test/java/day_12_files_seleniumWait/C03UploadFile.java
+++ src/test/java/day_12_files_seleniumWait/C03UploadFile.java
@@ -1,30 +1,43 @@
 package day_12_files_seleniumWait;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class C03UploadFile extends TestBase {
 
+    @Test
+    public void test01() {
+        driver.get("https://the-internet.herokuapp.com/upload");
+
+
+        //"C:\Users\hpvic\Downloads\b10 all test cases, code.docx"
+        //chooseFile butonuna basalim
+       WebElement coosFile = driver.findElement(By.id("file-upload"));
+
+        //Yuklemek istediginiz dosyayi secelim.
+        String dosyaYolu = System.getProperty("user.home") + "\\Downloads\\b10 all test cases, code.docx";
+
+        coosFile.sendKeys(dosyaYolu);
+
+        Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+
+        //Upload butonuna basalim.
+        driver.findElement(By.id("file-submit")).click();
 
 
 
+       String ActualTest = driver.findElement(By.xpath("//h3")).getText();
+
+       Assert.assertEquals("File Uploaded!",ActualTest);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
