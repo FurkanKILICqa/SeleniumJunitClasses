21bf9fffa1c403e0ee54c9bd8079dca5ddf31a85
Furkan Kılıc <fkrx928@gmail.com>
2023-11-15 13:12:51 +0300

    my commit

---
+++ src/test/java/day_13_seleniumwaits_expections/C03_NoSuchElementException.java
@@ -0,0 +1,17 @@
+package day_13_seleniumwaits_expections;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C03_NoSuchElementException extends TestBase {
+
+
+    @Test
+    public void test01() {
+
+
+
+
+
+    }
+}
