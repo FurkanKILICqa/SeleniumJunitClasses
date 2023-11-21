6178afd99b8652ec38dd023a6bedeb13a28e76f4
Furkan Kılıc <fkrx928@gmail.com>
2023-11-21 11:54:24 +0300

    my commit

--- src/test/java/day_16_extentreport_webtable_exel/C03_ReadExcel.java
+++ src/test/java/day_16_extentreport_webtable_exel/C03_ReadExcel.java
@@ -1,5 +1,18 @@
 package day_16_extentreport_webtable_exel;
 
+import org.junit.Test;
+
 public class C03_ReadExcel {
 
+
+    /*
+    1) Framework umuzde excel dosyalari ile calisabilmek icin oncelikle gerekli dependency ler pom xml e yuklememiz gerekir
+    2) Projemizde koymus oldugumuz excel dosyasinin yolunu belirmeliliy
+     */
+
+    @Test
+    public void test01() {
+        String path ="src/test/java/resources/Captals.xlsx";
+
+    }
 }
---
+++ src/test/java/day_17_excel/C02_ReadExcel.java
@@ -0,0 +1,20 @@
+package day_17_excel;
+
+import org.junit.Test;
+
+public class C02_ReadExcel {
+
+    //Örnek 1: Capitals.xlsx dosyasından 1. satır 2. sütundaki hücreyi yazdırın
+    //Örnek 2: 3. Satır 1. sütun değerini yazdırın ve "France" olduğunu test edin
+    //Örnek 3: Kullanılan satır sayısın bulun
+    //Örnek 4: Sayfadaki son satırin index ini yazdırınız
+    //Örnek 5: Excel sayfasindaki datalari COUNTRY,CAPITALS (Ülke-Başkent) key-value şeklinde console a yazdiriniz
+    // {{USA,D.C},{FRANCE,PARIS}....}
+
+    @Test
+    public void test01() {
+
+
+
+    }
+}
