94adcc1d93d87e58c7fffe791828211476e615fe
Furkan Kılıc <fkrx928@gmail.com>
2023-11-21 13:26:14 +0300

    my commit

--- src/test/java/day_17_excel/C03_ExcelTest.java
+++ src/test/java/day_17_excel/C03_ExcelTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 import java.io.FileInputStream;
@@ -13,26 +14,26 @@ import java.io.IOException;
 
 public class C03_ExcelTest extends TestBase {
 
-    //https://www.bluerentalcars.com/ sayfasına gidelim
-    //Login butonuna basalım
-    //Excel dosyasından aldığımız bir kullanıcı adı ve password ile login olalim
-    //Login olduğumuzu doğrulayalım
+
+
+
+
 
 
     @Test
     public void test01() throws IOException {
 
-
+        //https://www.bluerentalcars.com/ sayfasına gidelim
         driver.get("https://www.bluerentalcars.com/");
 
-
+        //Login butonuna basalım
         driver.findElement(By.xpath("//*[@class='btn btn-primary btn-sm']")).click();
 
         String path = "C:\\Users\\hpvic\\IdeaProjects\\B189SeleniumJunitDT\\src\\test\\java\\resources\\mysmoketestdata.xlsx";
         FileInputStream fis = new FileInputStream(path);
         Workbook workbook = WorkbookFactory.create(fis);
 
-
+        //Excel dosyasından aldığımız bir kullanıcı adı ve password ile login olalim
        String userMail = workbook.getSheet("customer_info").getRow(1).getCell(0).toString();
        String userPass = workbook.getSheet("customer_info").getRow(1).getCell(1).toString();
 
@@ -45,7 +46,10 @@ public class C03_ExcelTest extends TestBase {
         pass.sendKeys(userPass);
         driver.findElement(By.xpath("//*[@class='btn btn-primary']")).click();
 
-       // String ayhan = driver.findElement(By.xpath("dropdown-basic-button")).getText();
+        //Login olduğumuzu doğrulayalım
+        WebElement loginTest = driver.findElement(By.id("dropdown-basic-button"));
+        Assert.assertEquals("ayhan can",loginTest.getText());
+
 
 
 
