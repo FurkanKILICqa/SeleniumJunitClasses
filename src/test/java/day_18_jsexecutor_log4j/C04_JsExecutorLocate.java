c4bc926d9594182b4b6b5bb5187613c6dc3f682e
Furkan Kılıc <fkrx928@gmail.com>
2023-11-22 13:59:13 +0300

    my commit

--- src/test/java/day_18_jsexecutor_log4j/C04_JsExecutorLocate.java
+++ src/test/java/day_18_jsexecutor_log4j/C04_JsExecutorLocate.java
@@ -13,27 +13,52 @@ public class C04_JsExecutorLocate extends TestBase {
     @Test
     public void test01() {
 
-        //https://bonigarcia.dev/selenium-webdriver-java/shadow-dom.html   adresine gidelim
-        //Hello Shadow DOM elementini locate edelim
-
+        ///https://bonigarcia.dev/selenium-webdriver-java/shadow-dom.html   adresine gidelim
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/shadow-dom.html");
 
-     // String shadow = driver.findElement(By.xpath("//*[@class='display-6']")).getText();
-     //  Assert.assertEquals("Shadow DOM",shadow);
 
+        //Hello Shadow DOM elementini locate edelim
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("return document.getElementById(\"content\");");
+        WebElement helloShadowDomWe = (WebElement) js.executeScript("return document.getElementById('content');");
+        Assert.assertEquals("Hello Shadow DOM",helloShadowDomWe.getText());
 
     }
 
     @Test
     public void amazonTest() {
 
-        driver.get("https://www.amazon.com.tr/");
+        //amazon a gidin
+        driver.get("https://amazon.com");
 
-        WebElement writecell = driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
-        writecell.sendKeys("selenium");
+
+        //arama kutusunda selenium yazin
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("selenium");
+
+        /*
+        <input type="text" id="twotabsearchtextbox" value="" name="field-keywords" autocomplete="off" placeholder="Search Amazon"
+        class="nav-input nav-progressive-attribute" dir="auto" tabindex="0" aria-label="Search Amazon" spellcheck="false">
+         */
+
+        //arama kutusuna basarili bir sekilde yazinin gönderildigini test edin
+
         JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        String searchBoxValue =js.executeScript("return document.getElementById('twotabsearchtextbox').value").toString();
+        System.out.println("searchBoxValue = " + searchBoxValue);
+        Assert.assertEquals("selenium",searchBoxValue);
+
+
+
+        String typeAttributeDegeri =js.executeScript("return document.getElementById('twotabsearchtextbox').type").toString();
+        System.out.println(typeAttributeDegeri);
+
+        String nameAttributeDegeri =js.executeScript("return document.getElementById('twotabsearchtextbox').name").toString();
+        System.out.println(nameAttributeDegeri);
+
+
+        String classAttributeDegeri =js.executeScript("return document.getElementById('twotabsearchtextbox').className").toString();
+        System.out.println(classAttributeDegeri);
 
 
 
