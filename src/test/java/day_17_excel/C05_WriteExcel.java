6073f264e332518c25ef393b5add55c9c62f0a2f
Furkan Kılıc <fkrx928@gmail.com>
2023-11-23 04:06:20 +0300

    my commit

--- src/test/java/day_17_excel/C05_WriteExcel.java
+++ src/test/java/day_17_excel/C05_WriteExcel.java
@@ -1,14 +1,15 @@
 package day_17_excel;
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class C05_WriteExcel {
 
@@ -22,25 +23,40 @@ public class C05_WriteExcel {
         FileInputStream fis = new FileInputStream(path);
         Workbook workbook = WorkbookFactory.create(fis);
         Sheet sheet = workbook.getSheet("customer_info");
-        Row row = sheet.getRow(0);
 
-        sheet.getRow(0).createCell(3).setCellValue("Name");
-        sheet.getRow(1).createCell(3).setCellValue("ayhan");
-        sheet.getRow(2).createCell(3).setCellValue("beyhan");
-        sheet.getRow(3).createCell(3).setCellValue("ceyhan");
-        sheet.getRow(4).createCell(3).setCellValue("ali");
-        sheet.getRow(5).createCell(3).setCellValue("veli");
-        sheet.getRow(6).createCell(3).setCellValue("ahmet");
-        sheet.getRow(7).createCell(3).setCellValue("can");
-        sheet.getRow(8).createCell(3).setCellValue("sam");
-        sheet.getRow(9).createCell(3).setCellValue("kate");
-        sheet.getRow(10).createCell(3).setCellValue("raj");
-        sheet.getRow(11).createCell(3).setCellValue("pam");
 
-        FileOutputStream fos = new FileOutputStream(path);
-        workbook.write(fos);
-        fos.close();
-        fis.close();
+       // sheet.getRow(0).createCell(3).setCellValue("Name");
+       // sheet.getRow(1).createCell(3).setCellValue("ayhan");
+       // sheet.getRow(2).createCell(3).setCellValue("beyhan");
+       // sheet.getRow(3).createCell(3).setCellValue("ceyhan");
+       // sheet.getRow(4).createCell(3).setCellValue("ali");
+       // sheet.getRow(5).createCell(3).setCellValue("veli");
+       // sheet.getRow(6).createCell(3).setCellValue("ahmet");
+       // sheet.getRow(7).createCell(3).setCellValue("can");
+       // sheet.getRow(8).createCell(3).setCellValue("sam");
+       // sheet.getRow(9).createCell(3).setCellValue("kate");
+       // sheet.getRow(10).createCell(3).setCellValue("raj");
+       // sheet.getRow(11).createCell(3).setCellValue("pam");
+//
+       // FileOutputStream fos = new FileOutputStream(path);
+       // workbook.write(fos);
+       // fos.close();
+       // fis.close();
+
+
+        List<String> names = Arrays.asList("ayhan","beyhan","ceyhan","ali","veli","ahmet","can","sam","kate","raj","pam");
+
+
+        for (int i = 0; i < names.size(); i++) {
+
+            Row row = sheet.createRow(i + 1);
+
+            Cell cell = row.createCell(3);
+
+            cell.setCellValue(names.get(i));
+        }
+
+
 
 
 
